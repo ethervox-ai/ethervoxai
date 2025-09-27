5812a79701db90fe1efdec4a4347a3c032144146
mkostersitz <mkostersitz@mac.com>
2025-09-26 17:35:19 -0700

    Fixing 2 additional license headers

--- dashboard/src/stores/system.js
+++ dashboard/src/stores/system.js
@@ -1,3 +1,19 @@
+/**
+ * @file stores.js
+ * @brief Vuex store module for managing application state in EthervoxAI Dashboard
+ * 
+ * Copyright (c) 2024-2025 EthervoxAI Team
+ * 
+ * This file is part of EthervoxAI, licensed under CC BY-NC-SA 4.0.
+ * You are free to share and adapt this work under the following terms:
+ * - Attribution: Credit the original authors
+ * - NonCommercial: Not for commercial use
+ * - ShareAlike: Distribute under same license
+ * 
+ * For full license terms, see: https://creativecommons.org/licenses/by-nc-sa/4.0/
+ * SPDX-License-Identifier: CC-BY-NC-SA-4.0
+ */
+
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import axios from 'axios'
