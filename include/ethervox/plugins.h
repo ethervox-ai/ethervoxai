d94a8cfe91210c9949a40ba95466c22313f9df9a
mkostersitz <mkostersitz@mac.com>
2025-09-26 17:31:29 -0700

    Add license headers to all source files
    - Standardized CC-BY-NC-SA-4.0 license headers across all files
    - Added SPDX identifiers and copyright notices
    - Includes proper attribution and license terms
    - Automated bulk addition using PowerShell script
    - Covers C/C++ source files and JavaScript dependencies

--- include/ethervox/plugins.h
+++ include/ethervox/plugins.h
@@ -1,3 +1,18 @@
+/**
+ * @file plugins.h
+ * @brief Plugin system interface definitions for EthervoxAI
+ * 
+ * Copyright (c) 2024-2025 EthervoxAI Team
+ * 
+ * This file is part of EthervoxAI, licensed under CC BY-NC-SA 4.0.
+ * You are free to share and adapt this work under the following terms:
+ * - Attribution: Credit the original authors
+ * - NonCommercial: Not for commercial use
+ * - ShareAlike: Distribute under same license
+ * 
+ * For full license terms, see: https://creativecommons.org/licenses/by-nc-sa/4.0/
+ * SPDX-License-Identifier: CC-BY-NC-SA-4.0
+ */
 #ifndef ETHERVOX_PLUGINS_H
 #define ETHERVOX_PLUGINS_H
 
--- sdk/ethervox_sdk.h
+++ sdk/ethervox_sdk.h
@@ -1,3 +1,18 @@
+/**
+ * @file ethervox_sdk.h
+ * @brief EthervoxAI SDK header definitions
+ * 
+ * Copyright (c) 2024-2025 EthervoxAI Team
+ * 
+ * This file is part of EthervoxAI, licensed under CC BY-NC-SA 4.0.
+ * You are free to share and adapt this work under the following terms:
+ * - Attribution: Credit the original authors
+ * - NonCommercial: Not for commercial use
+ * - ShareAlike: Distribute under same license
+ * 
+ * For full license terms, see: https://creativecommons.org/licenses/by-nc-sa/4.0/
+ * SPDX-License-Identifier: CC-BY-NC-SA-4.0
+ */
 #ifndef ETHERVOX_SDK_H
 #define ETHERVOX_SDK_H
 
